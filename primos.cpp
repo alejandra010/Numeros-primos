89826d1d41f1d51da333fd838a9690a747b63c70
Katherine1612 <52213235+Katherine1612@users.noreply.github.com>
2020-01-09 09:33:08 -0500

    Update primos.cpp
    estructura

--- primos.cpp
+++ primos.cpp
@@ -8,3 +8,24 @@ void hilo(){
 	outfile<<"Hola Mundo"<< std::endl <<"Alejandra"<<std::endl;
 	outfile.close();
 }
+int main(){
+
+int num;
+int i;
+int contador=0;
+
+printf("Ingrese un numero: ");
+scanf("%d",&num);
+
+for(i=num; i>0; i--)
+{
+if(num%i==0)
+contador++;
+}
+if(contador==2)
+printf("\n El numero que ingreso es primo");
+std::thread prueba(hilo);
+prueba.join();
+getch();
+}
+
